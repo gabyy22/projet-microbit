d064e065241046a9ea036a872abde60c0f00106c
gabyy22 <123979689+gabyy22@users.noreply.github.com>
2023-03-09 16:31:20 -0500

    m

--- main.ts
+++ main.ts
@@ -1,3 +1,41 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+    music.playTone(440, music.beat(BeatFraction.Whole))
 })
+input.onButtonPressed(Button.AB, function () {
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    music.playTone(392, music.beat(BeatFraction.Whole))
+})
+basic.showLeds(`
+    . # . # .
+    . . . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    `)
